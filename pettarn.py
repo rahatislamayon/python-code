136fe21abcfb2fa255134b9f38620c64b18a3da2
ayon <rahatislamayon@gmail.com>
2025-09-30 00:58:16 +0600

    Refactor pattern printing logic and add cs50 hello world script

--- pettarn.py
+++ pettarn.py
@@ -13,24 +13,24 @@ n = int(input_string)
 #     print()
 
 
-#     for i in range(n):
+# for i in range(n):
 #     for j in range(i,n):
 #         print("*", end=" ")
 #     print()
 
-# for i in range(n):
-#     for j in range(i,n):
-#         print("*", end=" ")
-#     for j in range(i+1):
-#         print(" ", end=" ")
-#     print()
+for i in range(n):
+    for j in range(i,n):
+        print("*", end=" ")
+    for j in range(i+1):
+        print(" ", end=" ")
+    print()
 
-# for i in range(n):
-#     for j in range(i,n):
-#         print(" ", end=" ")
-#     for j in range(i+1):
-#         print("*", end=" ")
-#     print()
+for i in range(n):
+    for j in range(i,n):
+        print(" ", end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
 
 
 # for i in range(n):
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
